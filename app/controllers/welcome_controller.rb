f2b7086054328a35345684a8464d693a4a52735b
Jose Murillo <josemurillo2017@gmail.com>
2018-06-06 21:48:59 -0500

    Reversed bootstrap forms

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,6 +14,10 @@ class WelcomeController < ApplicationController
 
     #Listing all the risks
     missing_sat = [@member.grade > 11 & @member.sat_score = 0, 1] # Does not have an SAT score even though in 12th grade
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of bb26147... Fixed issue with flags not displaying
     low_grades  = [@member.gpa < 2,2] #GPA is below a 2.0
 
     #Combines all the risks into an array
@@ -28,11 +32,20 @@ class WelcomeController < ApplicationController
 
   def risk(risk_boolean, risk_id)
     #Checks if there is a flag
-    current_flag = Flag.where({member_id: @member.id.to_s , flag_id: risk_id.to_s})
+    current_flag = Flag.where(member_id = @member.id.to_s && flag_id =risk_id.to_s)
     flag_exists = !current_flag.empty?
+<<<<<<< HEAD
 
     if risk_boolean and !flag_exists
       #There is a risk and it has not been recorded
+=======
+    # puts "Checking if flag_exists"
+
+
+    if risk_boolean and !flag_exists
+      #There is a risk and it has not been recorded
+      # puts "There is a risk and it has not been recorded"
+>>>>>>> parent of bb26147... Fixed issue with flags not displaying
       new_flag = Flag.new
       new_flag.flag_id = risk_id
       new_flag.member_id = @member.id
@@ -40,9 +53,13 @@ class WelcomeController < ApplicationController
       new_flag.save
 
     elsif !risk_boolean and flag_exists
-
       #There is no risk so we need to delete the flag
+<<<<<<< HEAD
       current_flag.destroy_all
+=======
+      # puts "There is no risk so we need to delete the flag"
+      current_flag.first.destroy
+>>>>>>> parent of bb26147... Fixed issue with flags not displaying
     end
   end
 
