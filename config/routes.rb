5ebe44e406fd266c7b9606c54e8d33aaa3bed633
Jose Murillo <josemurillo2017@gmail.com>
2018-06-07 23:03:40 -0500

    Added form to partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'testing/test1'
 
   get 'welcome/index' => "welcome#index"
+  patch 'welcome/index' => "welcome#index"
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
