81680f24005c7b5873710a45a215baa3584f5e98
Jose Murillo <josemurillo2017@gmail.com>
2018-07-08 21:19:18 -0700

    Created dashboard and home page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'welcome/index' => "welcome#index"
   get "welcome/risk" => "welcome#risk_page"
   get "welcome/solution" => "welcome#solution_page"
+  get "welcome/dashboard" => "welcome#dashboard"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
